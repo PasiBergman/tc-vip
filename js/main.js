ab5bb251c9701a1c73b46313b6e44744941dd877
Pasi Bergman <pasi.bergman@iki.fi>
2013-05-16 01:22:15 +0300

    Bootstrap otettu käyttöön.

---
+++ js/main.js
@@ -0,0 +1,16 @@
+/*
+function equalHeight(group) {
+	var tallest = 0;
+	group.each(function() {
+		var thisHeight = $(this).height();
+		if(thisHeight > tallest) {
+			tallest = thisHeight;
+		}
+	});
+	group.height(tallest);
+}
+
+$(document).ready(function() {
+	equalHeight($(".vip-article"));
+});
+*/
